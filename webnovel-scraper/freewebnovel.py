43033361df2657e27b368b168fbc0baff7672528
otvegg <timian@husveg.net>
2024-05-20 19:12:42 +1000

    Trying to make it more modular

--- webnovel-scraper/freewebnovel.py
+++ webnovel-scraper/freewebnovel.py
@@ -3,20 +3,19 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd  # remove if i can make nice tables
 from alive_progress import alive_bar, config_handler
-import to_file
 
 config_handler.set_global(spinner="waves")
 
+URL = "https://freewebnovel.com"
+SEARCHURL = "https://freewebnovel.com/search/"
+CHAPTERBASEURL = "https://freewebnovel.comenovel.com"
 
-allowed_formats = ["txt"]  # To add: epub, pdf
 
+def search_novels(searchString: str):
 
-def search_novels(searchstring: str):
-    url = "https://freewebnovel.com"
-    searchUrl = url + "/search/"
-    payload = {"searchkey": searchstring}
+    payload = {"searchkey": searchString}
 
-    response = requests.post(searchUrl, data=payload)
+    response = requests.post(SEARCHURL, data=payload)
     soup = BeautifulSoup(response.text, "html.parser")
     elements = soup.select(
         "body > div.main > div.wp > div.row-box > div.col-content > div > div > div > div > div.txt"
@@ -35,75 +34,27 @@ def search_novels(searchstring: str):
         else:
             n_chapters = n_chapters[0].get_text(strip=True).split()[0]
 
-        website = url.split("/")[2]
+        firstChapter = CHAPTERBASEURL + link.replace(".html", "/chapter-1")
 
-        table.append([title, score, n_chapters, website, link])
+        table.append([title, score, n_chapters, URL, firstChapter])
 
-    df = pd.DataFrame(table, columns=["Title", "Score", "Chapters", "Website", "Link"])
-    df.index += 1
-    return df
+    return table
 
 
-df = search_novels("martial world")
+# def get_novel_url(novel_info):
+#     if novel_info.Website == "freewebnovel.com":
+#         link = novel_info.Link.replace(".html", "/chapter-1")
+#     else:
+#         website = novel_info.Website
+#         link = novel_info.Link
+#     return chapterBaseUrl, chapterBaseUrl + link
 
 
-def select_novel(df: pd.DataFrame):
-    print(df)
+def get_chapter_info(chapterUrl: str, nChapters: int):
 
-    # Get min and max of possible values
-    min_value = df.index.min()
-    max_value = df.index.max()
-
-    # Ask for value from user
-    selected_novel = -1
-    while selected_novel > max_value or selected_novel < min_value:
-        selected_novel = int(
-            input("Enter the number corresponding to the novel you want to read: ")
-        )
-        if selected_novel > max_value or selected_novel < min_value:
-            print(
-                "Incorrect value, please choose a value fitting corresponding to a novel."
-            )
-
-    # Retrieve selected novel
-    novel_info = df.loc[selected_novel]
-
-    print("Selected:", novel_info.Title, "with rating", novel_info.Score)
-    return novel_info
-
-
-novel_info = select_novel(df)
-
-
-def select_file_format():
-    file_format = -1
-    while file_format not in allowed_formats:
-        file_format = input("Enter the wanted file format: ")
-        if file_format not in allowed_formats:
-            print(f"Please choose between [{', '.join(allowed_formats)}].")
-    return file_format
-
-
-file_format = select_file_format()
-
-
-def get_novel_url():
-    if novel_info.Website == "freewebnovel.com":
-        website = "https://freewebnovel.comenovel.com"
-        link = novel_info.Link.replace(".html", "/chapter-1")
-    else:
-        website = novel_info.Website
-        link = novel_info.Link
-    return website, website + link
-
-
-website, novel_url = get_novel_url()
-
-
-def get_chapter_info(url: str):
-    response = requests.get(url)
+    response = requests.get(chapterUrl)
     soup = BeautifulSoup(response.text, "html.parser")
-    next_chapter = website + soup.select_one(
+    next_chapter = CHAPTERBASEURL + soup.select_one(
         "#main1 > div > div > div.ul-list7 > ul > li:nth-child(4) > a"
     ).get("href")
     current_chapter = soup.select_one("#main1 > div > div > div.top > span").get_text(
@@ -111,7 +62,7 @@ def get_chapter_info(url: str):
     )
 
     # Check if the current chapter is the last chapter
-    if url.split("-")[-1] == novel_info.Chapters:
+    if int(chapterUrl.split("-")[-1]) == nChapters:
         next_chapter = -1
 
     content = soup.select("#article > p")
@@ -119,25 +70,6 @@ def get_chapter_info(url: str):
     for element in content:
         paragraphs.append(element.get_text(strip=True))
 
-    # TODO check if the website is freewebnovel.com
     paragraphs = paragraphs[:-1]  # Remove last paragraph (advertisement)
 
     return current_chapter, paragraphs, next_chapter
-
-
-# Create file
-filePath = to_file.create_file(file_format, f'{novel_info.Title.replace(" ", "-")}.txt')
-
-if filePath == -1:
-    print("Error creating file")
-else:
-    with alive_bar(int(novel_info.Chapters), title="Chapters") as bar:
-        while True:
-            current_chapter, paragraphs, novel_url = get_chapter_info(novel_url)
-            # append to file
-            to_file.append_file(file_format, filePath, current_chapter, paragraphs)
-            bar()
-
-            # Reach last chapter, so we exit
-            if novel_url == -1:
-                break
--- webnovel-scraper/webscraper.py
+++ webnovel-scraper/webscraper.py
@@ -1,1 +1,41 @@
-# Readme
+from alive_progress import alive_bar
+import websites
+import helpers
+import to_file
+
+
+def initiate():
+    # Search through possible
+    results = websites.search_websites("martial world")
+
+    novel_info = helpers.select_novel(results)
+
+    file_format = helpers.select_file_format()
+    if file_format == -1:
+        print("Error with fileformat")
+        return -1
+
+    # Create file
+    filePath = to_file.create_file(
+        file_format, f'{novel_info.Title.replace(" ", "-")}.txt'
+    )
+    if filePath == -1:
+        print("Error creating file")
+        return -1
+
+    novel_url = novel_info.ChapterUrl
+    with alive_bar(int(novel_info.Chapters), title="Chapters") as bar:
+        while True:
+            current_chapter, paragraphs, novel_url = websites.get_chapter_info(
+                novel_info, novel_url
+            )
+            # append to file
+            to_file.append_file(file_format, filePath, current_chapter, paragraphs)
+            bar()
+
+            # Reach last chapter, so we exit
+            if novel_url == -1:
+                break
+
+
+initiate()
